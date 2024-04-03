95e772e49929ee1e9b8f49c10b08be8d5b62a346
Asik8 <motasembillah84@gmail.com>
2024-04-03 22:19:22 +0600

    first problem solved

--- Problem_1.c
+++ Problem_1.c
@@ -1,22 +1,15 @@
-#include<stdio.h>
-#include<math.h>
-#include<string.h>
-#include<ctype.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define lli long long int
 #define max_size 100000
 
 int main()
 {
-    int t;
-    scanf("%d",&t);
-
-    while(t--)
-    {
-        printf("\n");
-    }
-
+    printf("I Love Practice\n");
     return 0;
 }
