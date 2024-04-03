e7ddd2f3f5b99da0407c4ab504628afff2c6b03d
Asik8 <motasembillah84@gmail.com>
2024-04-03 22:16:48 +0600

    First problem added

---
+++ Problem_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define lli long long int
+#define max_size 100000
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+
+    while(t--)
+    {
+        printf("\n");
+    }
+
+    return 0;
+}
